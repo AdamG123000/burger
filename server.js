2ab1ee98f84823c8b8ec3e4d8198fb22ae90decb
Adam Grodahl <adamgrodahl@Adams-MBP.hsd1.or.comcast.net>
2019-07-25 14:45:26 -0700

    routes working

--- server.js
+++ server.js
@@ -2,3 +2,23 @@ const express = require("express");
 const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
+
+const routes = require("./routes/handlers");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json()); 
+app.use(methodOverride("_method"));
+
+//Configure Express Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.use("/", routes);
+
+//start the server
+app.listen(PORT, () => {
+    console.log('Server is starting at PORT ${PORT}');
+});
